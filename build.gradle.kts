4fc962fb660dbb423894751b893313d0b9275aeb
Paul Spiesberger <spiesberger-hoeckner@tugraz.at>
2025-05-23 14:03:38 +0200

    Add first draft of github actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,6 @@ plugins {
 subprojects {
     apply(plugin = "org.jlleitschuh.gradle.ktlint") // Version should be inherited from parent
 
-    repositories {
-        // Required to download KtLint
-        mavenCentral()
-    }
-
     // Optionally configure plugin
     configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
         debug.set(true)
