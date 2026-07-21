89b4b592a2f650381180c78bbaf03ce097bd578c
Muhammad Haris Sabil Al Karim <harissabil04@gmail.com>
2026-07-21 19:11:03 +0700

    Rename prompt strategies for clarity

--- shared/src/commonMain/kotlin/org/catrobat/aitutor/domain/prompt/PromptVersion.kt
+++ shared/src/commonMain/kotlin/org/catrobat/aitutor/domain/prompt/PromptVersion.kt
@@ -5,6 +5,6 @@ enum class PromptVersion(val displayName: String, val strategy: PromptStrategy) 
     V2("VSCode/Copilot Style", V2PromptStrategy()),
     V3("Cursor AI Style", V3PromptStrategy()),
     V4("Trae AI Style", V4PromptStrategy()),
-    POCKET_CODE_SPRITE("PocketCode Sprite", PocketCodeSpriteEditorPromptStrategy()),
-    EMBROIDERY_DESIGNER_SPRITE("Embroidery Designer Sprite", EmbroideryDesignerSpriteEditorPromptStrategy()),
+    POCKET_CODE_SPRITE_EDITOR("PocketCode Sprite Editor", PocketCodeSpriteEditorPromptStrategy()),
+    EMBROIDERY_DESIGNER_SPRITE_EDITOR("Embroidery Designer Sprite Editor", EmbroideryDesignerSpriteEditorPromptStrategy()),
 }
